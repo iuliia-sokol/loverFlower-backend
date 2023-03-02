c952798375fd9cae103ac4d883aa25592069469c
Iuliia Sokol <avare779@gmail.com>
2023-03-02 18:12:00 +0200

    edited routes

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
-
+const { validateBody, isValidId } = require("../../middlewares");
 const ctrl = require("../../controllers/products");
+const { schemas } = require("../../models/product");
 
 router.get("/", ctrl.getAll);
+
+router.get("/:id", isValidId, ctrl.getById);
+
+router.post("/", validateBody(schemas.addSchema), ctrl.add);
+
+router.delete("/:id", isValidId, ctrl.deleteById);
 
 module.exports = router;
